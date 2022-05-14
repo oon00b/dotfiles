e4aee0a980a827fde7e3bc628e1e9bd0a2a3f307
oon00b <60419766+oon00b@users.noreply.github.com>
2022-05-15 03:24:52 +0900

    swayのステータスバー用のスクリプトをアップデート

--- .bin/setstatusbar.sh
+++ .bin/setstatusbar.sh
@@ -10,20 +10,56 @@ else
     exit
 fi
 
-echo "{\"version\":1}"
-echo "["
+status_commands=""
+add_status_commands(){
+    for comm in ${@} ; do
+        status_commands="${status_commands} ${comm}"
+    done
+    status_commands=${status_commands##" "}
+}
+
+audio_status(){
+    pulse_sink="$(pactl get-default-sink)"
+    pulse_volume="$(${HOME}/.bin/volctl.sh get-volume)"
+    echo "${pulse_sink}:${pulse_volume}"
+}
+add_status_commands "audio_status"
+
+battery_path="/sys/class/power_supply/BAT0"
+battery_status(){
+    capacity="$(cat "${battery_path}/capacity")"
+    is_charging="$(grep -i -s -q "Charging" "${battery_path}/status" && echo "+" || echo "-")"
+    echo "BAT:${capacity}%[${is_charging}]"
+}
+test -d ${battery_path} && add_status_commands "battery_status"
+
+wifi_interface_path="/sys/class/net/wlp3s0"
+wifi_status(){
+    operational_state="$(cat "${wifi_interface_path}/operstate")"
+    echo "wl:${operational_state}"
+}
+test -d ${wifi_interface_path} && add_status_commands "wifi_status"
+
+date_status(){
+    echo "$(date "+%Y-%m-%d(%a) %H:%M")"
+}
+add_status_commands "date_status"
+
+echo '{"version":1}'
+echo '['
 
 send_status(){
-    pulse_status="$(pactl get-default-sink):$(${HOME}/.bin/volctl.sh get-volume)"
-    wifi_status="wl:$(cat /sys/class/net/wlp3s0/operstate)"
-    date_status="$(date "+%Y-%m-%d(%a) %H:%M")"
-
-    echo "[{\
-\"full_text\":\"${pulse_status}\",
-\"separator\":true},{\
-\"full_text\":\"${wifi_status}\",
-\"separator\":true},{\
-\"full_text\":\"${date_status}\"}],"
+    body="["
+    i=1
+    cnt="$(echo ${status_commands} | wc -w)"
+    while test "${i}" -le "${cnt}" ; do
+        comm="$(echo "${status_commands}" | cut -f ${i} -d " ")"
+        body="${body}{\"full_text\":\"$(eval ${comm})\", \"separator\":true}"
+        test "${i}" -ne "${cnt}" && body="${body},"
+        i="$((${i} + 1))"
+    done
+    #body="${body}{\"full_text\":\"foo\", \"separator\":true}"
+    echo "${body}],"
 }
 
 while true ; do
